b6cc57cd69839381fbc8bdd4df3620406094b6d5
Bryan <bryan.ortiz@ug.uchile.cl>
2019-10-13 10:55:43 -0300

    Version 2.0

---
+++ src/model/tactician/Tactician.java
@@ -0,0 +1,13 @@
+package model.tactician;
+
+/**
+ * Controller of the game.
+ * The controller manages all the input received from de game's GUI.
+ *
+ * @author Bryan Ortiz P
+ * @version 2.1
+ * @since 2.1
+ */
+public class Tactician {
+
+}
